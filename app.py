f0d920ca0a1f473ef0cebd4f52146b9a07f9d304
saiprakash1807 <saiprakash1807@gmail.com>
2025-04-26 23:42:29 +0530

    added api keys to env

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 import streamlit as st
+from dotenv import load_dotenv
 
 
 def main():
+    load_dotenv()
     st.set_page_config(page_title="chat with pdfs",page_icon=":books:")
     st.header("Chat with multiple PDFs :books:")
     st.text_input("Ask a question about attached pdfs:")
