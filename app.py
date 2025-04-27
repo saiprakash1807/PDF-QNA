bc1a68fdbfeacdbafb2cfc72764499e6fe04a644
saiprakash1807 <saiprakash1807@gmail.com>
2025-04-27 23:04:47 +0530

    extracted and displayed pdf content

--- app.py
+++ app.py
@@ -1,5 +1,16 @@
 import streamlit as st
 from dotenv import load_dotenv
+from PyPDF2 import PdfReader
+
+
+def get_pdf_text(pdf_docs):
+    text=""
+    for pdf in pdf_docs:
+        pdf_reader = PdfReader(pdf)
+        for page in pdf_reader.pages:
+            text+=page.extract_text()
+    return text
+
 
 def main():
     load_dotenv()
@@ -17,6 +28,8 @@ def main():
         if st.button("Process"):
             with st.spinner("Processing"):
                 # get pdf text
+                raw_text  = get_pdf_text(pdf_docs)
+                st.write(raw_text)
                 # get the text chunks
                 # create vector storage
                 pass  # <--- added 'pass' to avoid syntax error for now
